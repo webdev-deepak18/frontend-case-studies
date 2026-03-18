527f71c8113d7a2e318f967d6bac39b3e154bafe
Deepak S <webdev.deepak18@gmail.com>
2026-03-18 16:46:18 +0530

    add redirect from portfolio v1 to v2

--- 04-portfolio-website/next.config.js
+++ 04-portfolio-website/next.config.js
@@ -3,6 +3,17 @@
  * @type {import('next').NextConfig}
  */
 
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination:
+          'https://deepak-design-engineer-portfolio.vercel.app/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
